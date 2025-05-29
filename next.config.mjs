20a31746c8c2f6113368b213433007e512dbae88
VIN | @vinflipside <133009613+akirausethis@users.noreply.github.com>
2025-05-29 12:12:17 +0700

    Done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+                protocol: 'https',
+                hostname: 'i.pinimg.com', // Untuk gambar Pinterest
+            },
+            {
+                protocol: 'https', // Pastikan Picsum.photos menggunakan HTTPS
+                hostname: 'picsum.photos', // Tambahkan domain ini
+            },
+            {
+                protocol: 'http', // Jika Anda menggunakan localhost dengan HTTP
+                hostname: 'localhost', // Untuk pengembangan lokal
+            },
+            // Anda bisa menambahkan domain Anda sendiri di sini jika dibutuhkan
+            // {
+            //   protocol: 'https',
+            //   hostname: 'yourdomain.com', // Domain Anda sendiri
+            // },
+        ],
+    },
+};
 
 export default nextConfig;
